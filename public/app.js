1bf038e1362cb73ece7efc386929f0449f3fbad0
DESTINY <moregiedestiny2@gmail.com>
2024-12-19 03:17:15 +0100

    first commit

---
+++ public/app.js
@@ -0,0 +1,15 @@
+const tg = window.Telegram.WebApp;
+
+tg.expand();
+
+
+const user = tg.initDataUnsafe?.user || {};
+
+
+document.getElementById('welcome').insertAdjacentHTML(
+    'afterbegin',
+    `<h1>Welcome, ${user.first_name || 'User'}!</h1>
+    ${user.photo_url
+      ? `<img src="${user.photo_url}" alt="${user.first_name}'s profile picture">`
+      : `<p>No profile picture available.</p>`}`
+);
